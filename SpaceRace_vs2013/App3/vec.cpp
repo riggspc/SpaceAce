443abf6c5a39a4ca9055ac7b577c2cb1b320eb93
polpolion <chvink@umich.edu>
2014-02-20 09:59:07 -0500

    something is broken

---
+++ SpaceRace_vs2013/App3/vec.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include "vec.h"
+
+
+vec3d::vec3d(float _x, float _y, float _z)
+:	x(_x),
+	y(_y),
+	z(_z)
+{
+}
---
+++ SpaceRace_vs2013/App3/vec.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// TODO: add useful math functionality as needed
+
+struct vec3d
+{
+	vec3d(float _x = 0.0f, float _y = 0.0f, float _z = 0.0f);
+
+	float x, y, z;
+};
+
